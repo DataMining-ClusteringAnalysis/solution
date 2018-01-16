c7528f1de62077f08079aa12187ef79cda144520
Xin Huang <xxinhuang61@gmail.com>
2018-01-16 12:31:08 -0600

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -9,4 +9,39 @@ GROUP BY city_name
 
 
 
+
+SELECT city_name, dayOfWeek, SUM(CASE WHEN status = 'completed' AND
+                                           DATE_PART('day', request_at - _ts)*24 + DATE_PART('hour', request_at - _ts) <= 168 THEN 1
+                                      ELSE 0
+                                 END)/COUNT(*)*100 AS percentOfSignup
+
+FROM (SELECT *, (CASE WHEN EXTRACT(DOW, _ts) = 1 THEN 'Mon'
+                      WHEN EXTRACT(DOW, _ts) = 2 THEN 'TUE'
+                      WHEN EXTRACT(DOW, _ts) = 3 THEN 'WED'
+                      WHEN EXTRACT(DOW, _ts) = 4 THEN 'THU'
+                      WHEN EXTRACT(DOW, _ts) = 5 THEN 'FRI'
+                      WHEN EXTRACT(DOW, _ts) = 6 THEN 'SAT'
+                      WHEN EXTRACT(DOW, _ts) = 3 THEN 'SUN'
+                 END) AS dayOfWeek
+      FROM trips LEFT JOIN cities on trips.city_id = cities.city_id
+                 LEFT JOIN events on trips.client_id = events.rider_id
+      WHERE DATE_PART('year', _ts) = 2016 AND
+            DATE_PART('week', _ts) = 1 AND
+            city_name IN ('Qarth', 'Meereen') AND
+            event_name = 'sign_up_success'
+      )
+      
+GROUP BY city_name, dayOfWeek
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     )
+
+
       
