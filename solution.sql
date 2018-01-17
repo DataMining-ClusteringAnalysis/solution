2a04d8f4c34ed54805a3570359649830ef99e9be
Xin Huang <xxinhuang61@gmail.com>
2018-01-17 00:18:44 -0600

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -10,11 +10,11 @@ GROUP BY city_name;
 
 
 
-SELECT city_name, dayOfWeek, SUM(CASE WHEN status = 'completed' AND
+SELECT city_name, dayOfWeek, (SUM(CASE WHEN status = 'completed' AND
                                            DATE_PART('day', request_at - _ts)*24 + DATE_PART('hour', request_at - _ts) <= 168
                                            THEN 1
                                       ELSE 0
-                                 END)/COUNT(*)*100 AS percentOfSignup
+                                  END)/COUNT(*))*100 AS percentOfSignup
 
 FROM (SELECT *, (CASE WHEN EXTRACT(DOW FROM _ts) = 1 THEN 'Mon'
                       WHEN EXTRACT(DOW FROM _ts) = 2 THEN 'TUE'
@@ -24,15 +24,14 @@ FROM (SELECT *, (CASE WHEN EXTRACT(DOW FROM _ts) = 1 THEN 'Mon'
                       WHEN EXTRACT(DOW FROM _ts) = 6 THEN 'SAT'
                       WHEN EXTRACT(DOW FROM _ts) = 0 THEN 'SUN'
                  END) AS dayOfWeek
-      FROM trips LEFT JOIN cities on trips.city_id = cities.city_id
-                 LEFT JOIN events on trips.client_id = events.rider_id
-      WHERE DATE_PART('year', _ts) = 2016 AND
-            DATE_PART('week', _ts) = 1 AND
+      FROM trips LEFT JOIN cities on trips.city_id = cities.city_id LEFT JOIN events on trips.client_id = events.rider_id
+      WHERE EXTRACT(YEAR FROM _ts) = 2016 AND
+            EXTRACT(WEEK FROM _ts) = 1 AND
             city_name IN ('Qarth', 'Meereen') AND
             event_name = 'sign_up_success'
       )
       
-GROUP BY city_name, dayOfWeek
+GROUP BY city_name, dayOfWeek;     
      
      
      
